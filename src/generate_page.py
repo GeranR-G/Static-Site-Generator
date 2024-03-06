368ac90d1a3631d5ebdfdfedf82e6d11e3362be9
GeranR-G <rustgreeng@gmail.com>
2024-03-05 21:15:32 -0700

    cleaned up code based on solution samples

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,31 +1,30 @@
 import os
-from markdown_blocks import(
-     markdown_to_blocks,
-     markdown_to_html_node
-)
+from markdown_blocks import markdown_to_html_node
+
 
 def extract_title(markdown):
-    blocks = markdown_to_blocks(markdown)
-    for block in blocks:
-        if "# " in block:
-            return block.lstrip("# ").strip()
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
     raise ValueError("Invalid markdown: missing h1 header")
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
-    f = open(from_path)
+    f = open(from_path, "r")
     markdown = f.read()
     f.close()
-    f = open(template_path)
+
+    f = open(template_path, "r")
     template = f.read()
     f.close()
+
     markdown_html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    template.replace("{{ Title }}", title)
-    template.replace("{{ Content }}", markdown_html)
+    template = template.replace("{{ Title }}", title).replace("{{ Content }}", markdown_html)
+
     dest_path_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_path_dir):
         os.makedirs(dest_path_dir)
     new_HTML = open(dest_path, "w")
     new_HTML.write(template)
-    new_HTML.close()
--- src/main.py
+++ src/main.py
@@ -2,18 +2,25 @@ import os, shutil
 from copystatic import copy_directory
 from generate_page import generate_page
 
-static_path = "./static/"
-destination_path = "./public/"
-index_path = "./content/index.md"
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    if os.path.exists(destination_path):
+    if os.path.exists(dir_path_public):
         print("Deleting public directory...")
-        shutil.rmtree(destination_path)
+        shutil.rmtree(dir_path_public)
+
     print("Copying static files to public directory...")
-    copy_directory(static_path, destination_path)
-    generate_page(index_path, template_path, destination_path+"index.html")
+    copy_directory(dir_path_static, dir_path_public)
+    
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html")
+    )
 
 main()
